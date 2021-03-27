34d699c3faa9503961d21621327b1a2d17641449
itstoasted-0 <snow.w686@gmail.com>
2021-03-27 12:23:55 +0900

    @16

--- task.rb
+++ task.rb
@@ -172,8 +172,8 @@ def q16
 
   # 以下に回答を記載
   
-  users.each_value do |name, age|
-    puts "私の名前は#{name}です。#{age}歳です。"
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
   end
 
 end
