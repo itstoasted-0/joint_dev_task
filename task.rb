90b8c3d5f2a9f593dec688866386dfc239c92685
itstoasted-0 <snow.w686@gmail.com>
2021-03-31 00:59:15 +0900

    Q17、Q18を修正

--- task.rb
+++ task.rb
@@ -195,9 +195,11 @@ class UserQ17
   end
   
   def info
-    puts "名前:#{@name}"
-    puts "年齢:#{@age}"
-    puts "性別:#{@gender}"
+    puts <<~TEXT
+    名前:#{@name}
+    年齢:#{@age}
+    性別:#{@gender}
+    TEXT
   end
 
 end
@@ -221,9 +223,9 @@ class UserQ18
   
   def introduce
     if @age > 10
-      puts "こんにちは,#{@name}と申します。宜しくお願いいたします。"
+      print "こんにちは,#{@name}と申します。宜しくお願いいたします。"
     else
-      puts "はいさいまいど〜,#{@name}です！！！"
+      print "はいさいまいど〜,#{@name}です！！！"
     end
   end
 
